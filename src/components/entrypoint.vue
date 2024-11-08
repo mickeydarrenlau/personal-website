5fa185db03680067ef11fd7e58857c70edafe0cf
mickeydarrenlau <darrenwjlau@gmail.com>
2024-11-08 23:52:53 +0800

    FIX: Prevent Vercel From Erroring

--- src/components/entrypoint.vue
+++ src/components/entrypoint.vue
@@ -11,5 +11,8 @@
 import "../assets/main.css"
 import Desktop from '/src/components/Desktop.vue';
 import Mobile from '/src/components/Mobile.vue';
-const isMobile = localStorage.mobile || window.navigator.maxTouchPoints > 1;
+let isMobile = false;
+onMounted( () => {
+isMobile = localStorage.mobile || window.navigator.maxTouchPoints > 1;
+})
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import entrypoint from '@/components/entrypoint.vue'
+//@ts-ignore
+import entrypoint from '../components/entrypoint.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
