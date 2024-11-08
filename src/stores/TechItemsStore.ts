848209588da8c3358620cf07ecb2e0da902cc563
Darren Lau Wei Jie <darrenwjlau@gmail.com>
2024-11-09 07:21:58 +0800

    Update TechItemsStore.ts

--- src/stores/TechItemsStore.ts
+++ src/stores/TechItemsStore.ts
@@ -2,20 +2,22 @@ import { defineStore } from 'pinia'
 export const useTechItems = defineStore('TechItems', {
     state: () => ({
       item: [
-        { name: 'Space Travel', type: 'Earbuds 🎧', maker: 'Moondrop', img: '/src/assets/tech_images/moondrop_space_travel.png' }
+        { name: 'Space Travel', type: 'Earbuds 🎧', maker: 'Moondrop', img: 'moondrop_space_travel.png' }
       ]
      }),
     getters: {
       items: (state) => {
+          let prefix = " /src/assets/tech_images/"
           if(import.meta.env.PROD) {
-              let items = state.item
-              for(let item in items) {
-                  item = items[item]
-                  item["img"].replace("/src","")
-              }
-              return items
+            prefix = "/assets/tech_images/"
           }
-          return state.item
+          let items = state.item
+          for(let item in items) {
+                  item = items[item]
+                  item["img"] = prefix + item["img"]
+          }
+          return items
+          
       },
     }
   })
