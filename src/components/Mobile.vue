421b75b887e1b00ff52a0dd56b05e4e03c1813d2
Darren Lau Wei Jie <darrenwjlau@gmail.com>
2024-11-09 04:41:35 +0800

    Update Mobile.vue

--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -22,7 +22,7 @@
         
         <BCard
         title="Playback History"
-        style="background-color: black; color: white; align-self: left;"
+        style="background-color: black; color: white; align-self: right;"
         >
                 <BTable show-empty :items="PresenceStore.play_history" style="" :fields="TableFieldStore.songs_field" :table-class="'table-dark .th-lg'" responsive>
                 <template #cell(songimg)="row">
