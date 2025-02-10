e372bc05d5a8e76d1d6bf734f2309bca6bd7f3e2
mickeydarrenlau <darrenwjlau@gmail.com>
2025-02-10 14:00:42 +0800

    ADD: Monitoring for Vercel

--- src/components/entrypoint.vue
+++ src/components/entrypoint.vue
@@ -5,6 +5,7 @@
   <template v-else>
     <Desktop />
   </template>
+  <SpeedInsights/>
 </template>
 
 <script setup>
@@ -12,4 +13,5 @@ import "../assets/main.css"
 import Desktop from '/src/components/Desktop.vue';
 import Mobile from '/src/components/Mobile.vue';
 let isMobile = localStorage.mobile || window.navigator.maxTouchPoints > 1;
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
