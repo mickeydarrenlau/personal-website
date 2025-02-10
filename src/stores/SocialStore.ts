016f46d0b9657b147fe476cc0e0b327bcc9f7ce9
mickeydarrenlau <darrenwjlau@gmail.com>
2025-02-10 15:41:56 +0800

    FINAL: SOCIAL

--- src/stores/SocialStore.ts
+++ src/stores/SocialStore.ts
@@ -10,14 +10,7 @@ export const useSocialStore = defineStore('SocialStore', {
             { link: 'https://github.com/mickeydarrenlau', img: 'github.png' },
             { link: 'https://www.instagram.com/mickeydarrenlau/', img: 'instagram.ico' },
             { link: 'https://www.reddit.com/user/RoundAccident3943', img: 'reddit.png'},
-            { link: 'mailto:darrenwjlau@gmail.com', img: 'gmail.png' },
-            { link: 'https://discordapp.com/users/718622831788949575', img: 'discord.ico' },
-            { link: 'https://github.com/mickeydarrenlau', img: 'github.png' },
-            { link: 'https://www.instagram.com/mickeydarrenlau/', img: 'instagram.ico' },
-            { link: 'https://www.reddit.com/user/RoundAccident3943', img: 'reddit.png'},
-            { link: 'mailto:darrenwjlau@gmail.com', img: 'gmail.png' },
-            { link: 'https://www.instagram.com/mickeydarrenlau/', img: 'instagram.ico' },
-            { link: 'https://www.reddit.com/user/RoundAccident3943', img: 'reddit.png'}
+            { link: 'mailto:darrenwjlau@gmail.com', img: 'gmail.png' }
         ],
       ]
      }),
